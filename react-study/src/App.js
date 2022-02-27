0d1461eb6a1307b2bbe59bf6ad8fec1185a71edf
benbpede <100111744+benbpede@users.noreply.github.com>
2022-02-27 19:38:52 +0900

    7장 라이프사이클 메서드

--- react-study/src/App.js
+++ react-study/src/App.js
@@ -7,8 +7,22 @@ import EventPractice from "./components/EventPractice";
 import ValidationSample from "./components/ValidationSample";
 import ScrollBox from "./components/ScrollBox";
 import IterationSample from "./components/IterationSample";
+import LifeCycleSample from "./components/LifeCycleSample";
+import ErrorBoundary from "./components/ErrorBoundary";
+
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: "#000000",
+  };
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
   render() {
     return (
       <div>
@@ -29,6 +43,10 @@ class App extends Component {
         </button>
         <br />
         <IterationSample />
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color} />
+        </ErrorBoundary>
       </div>
     );
   }
