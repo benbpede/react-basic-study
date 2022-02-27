4b656407f7fa07e95953b884494e9628bbc32dd6
benbpede <100111744+benbpede@users.noreply.github.com>
2022-02-27 17:05:04 +0900

    6장 컴포넌트 반복

--- react-study/src/App.js
+++ react-study/src/App.js
@@ -1,21 +1,36 @@
-import logo from "./logo.svg";
 import "./App.css";
-import MyComponent from "./MyComponent";
-import Counter from "./Counter";
-import Say from "./Say";
-import EventPractice from "./EventPractice";
+import { Component } from "react";
+import MyComponent from "./components/MyComponent";
+import Counter from "./components/Counter";
+import Say from "./components/Say";
+import EventPractice from "./components/EventPractice";
+import ValidationSample from "./components/ValidationSample";
+import ScrollBox from "./components/ScrollBox";
+import IterationSample from "./components/IterationSample";
 
-function App() {
-  return (
-    <>
-      <MyComponent name="React" favoriteNumber={1}>
-        children
-      </MyComponent>
-      <Counter></Counter>
-      <Say></Say>
-      <EventPractice></EventPractice>
-    </>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <MyComponent name="React" favoriteNumber={1}>
+          children
+        </MyComponent>
+        <Counter></Counter>
+        <Say></Say>
+        <EventPractice></EventPractice>
+        <ValidationSample />
+        <ScrollBox
+          ref={(ref) => {
+            this.scrollBox = ref;
+          }}
+        />
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+        <br />
+        <IterationSample />
+      </div>
+    );
+  }
 }
-
 export default App;
